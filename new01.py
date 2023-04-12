97cb1e95b90980b0d6a08662bfd53a8ce085a421
marcpan1998 <129896902+marcpan1998@users.noreply.github.com>
2023-04-12 23:45:50 +0800

    Create new01.py

---
+++ new01.py
@@ -0,0 +1,13 @@
+resultWorkerErr := make(chan error)
+defer close(resultWorkerErr)
+go func() {
+	defer cancel()
+	resultWorkerErr <- b.resultWorker(ctx)
+}()
+
+err := b.worker(ctx)
+cancel()
+if err == nil {
+	return <-resultWorkerErr
+}
+return multierror.Append(err, <-resultWorkerErr)
